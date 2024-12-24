13bc5a90c550883966a8616e11e44ac29c2ed4fd
amanueldemelash12 <amanueldemelash12@gmail.com>
2024-12-24 16:16:38 +0300

    delete account alert dialog

--- lib/features/host/features/profile/presentation/pages/account.dart
+++ lib/features/host/features/profile/presentation/pages/account.dart
@@ -37,7 +37,9 @@ class Account extends StatelessWidget {
               SizedBox(
                 width: 200,
                 child: CustomButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      _showDeleteDialog(context);
+                    },
                     style: ElevatedButton.styleFrom(
                         elevation: 0,
                         padding: EdgeInsets.all(1),
@@ -53,5 +55,50 @@ class Account extends StatelessWidget {
             ],
           ),
         ));
+  }
+
+  void _showDeleteDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        title: Text(
+          "Delete Account?",
+          style: Theme.of(context)
+              .textTheme
+              .bodyMedium!
+              .copyWith(fontWeight: FontWeight.bold),
+        ),
+        content: Text("This cant be undone."),
+        actionsAlignment: MainAxisAlignment.end,
+        actionsPadding: EdgeInsets.all(10),
+        actions: [
+          CustomButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(0.5),
+                  side: BorderSide(
+                      color:
+                          ColorConstant.secondBtnColor.withValues(alpha: 0.5)),
+                  backgroundColor: Colors.white),
+              child: Text("Cancel",
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: ColorConstant.secondBtnColor,
+                      ))),
+          CustomButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.only(left: 4, right: 4),
+                  backgroundColor: ColorConstant.red),
+              child: Text("delete",
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: Colors.white,
+                      )))
+        ],
+      ),
+    );
   }
 }
