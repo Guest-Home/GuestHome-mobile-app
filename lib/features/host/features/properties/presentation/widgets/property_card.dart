39eb3445f70eac4e75371caa1188e1f9c2701ed0
amanueldemelash12 <amanueldemelash12@gmail.com>
2024-12-24 13:21:08 +0300

    property card

--- lib/features/host/features/properties/presentation/widgets/property_card.dart
+++ lib/features/host/features/properties/presentation/widgets/property_card.dart
@@ -12,86 +12,105 @@ class PropertyCard extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       color: ColorConstant.cardGrey.withValues(alpha: 1.6),
       elevation: 0,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
+      child:  Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          spacing: 5,
-          children: [
-            Placeholder(
-              fallbackHeight: 200,
-            ),
-            ListTile(
-              title: Text(
-                'Property Name',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(fontWeight: FontWeight.bold),
+            mainAxisAlignment: MainAxisAlignment.start,
+            spacing: 5,
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network("https://media.architecturaldigest.com/photos/57e42deafe422b3e29b7e790/master/pass/JW_LosCabos_2015_MainExterior.jpg",
+                      fit:BoxFit.cover,
+                      width:MediaQuery.of(context).size.width,
+                      height: 210,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 10,
+                      left: 0,
+                      right: 0,
+                      child:Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: List.generate(3,(index) => Container(width: 10,
+                              height:10,
+                              margin: EdgeInsets.only(right: 5),
+                              decoration: BoxDecoration(color:ColorConstant.cardGrey,borderRadius: BorderRadius.circular(40)),),),) )
+                ],
               ),
-              subtitle: Text(
-                  'House Description goes here.Lorem ipsum dolor sit amet consectetur. Posuere vulputate gravida diam id feugiat. Suscipit et nunc tortor vivamus mattis sed est.'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(
-                  Icons.location_on_rounded,
-                  color: ColorConstant.primaryColor,
+              ListTile(
+                title: Text(
+                  'Property Name',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontWeight: FontWeight.bold),
                 ),
-                Text(
-                  'Addis Ababa, Ethiopia',
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: ColorConstant.inActiveColor,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Divider(
-              color: ColorConstant.inActiveColor.withValues(alpha: 0.2),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  spacing: 7,
-                  children: [
-                    Icon(
-                      Icons.other_houses_rounded,
-                      color: ColorConstant.primaryColor,
-                    ),
-                    Text(
-                      '10',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium!
-                          .copyWith(color: ColorConstant.primaryColor),
-                    ),
-                  ],
-                ),
-                RichText(
-                    text: TextSpan(
-                  text: 'Price: ',
-                  children: [
-                    TextSpan(
-                      text: 'ETB 1000',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: ColorConstant.primaryColor,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    TextSpan(
-                      text: '/night',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium!
-                          .copyWith(color: ColorConstant.inActiveColor),
-                    ),
-                  ],
-                )),
-              ],
-            ),
-          ],
+                subtitle: Text(
+                    'House Description goes here.Lorem ipsum dolor sit amet consectetur. Posuere vulputate gravida diam id feugiat. Suscipit et nunc tortor vivamus mattis sed est.'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.location_on_rounded,
+                    color: ColorConstant.primaryColor,
+                  ),
+                  Text(
+                    'Addis Ababa, Ethiopia',
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: ColorConstant.inActiveColor,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Divider(
+                color: ColorConstant.inActiveColor.withValues(alpha: 0.2),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    spacing: 7,
+                    children: [
+                      Icon(
+                        Icons.other_houses_rounded,
+                        color: ColorConstant.primaryColor,
+                      ),
+                      Text(
+                        '10',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(color: ColorConstant.primaryColor),
+                      ),
+                    ],
+                  ),
+                  RichText(
+                      text: TextSpan(
+                    text: 'Price: ',
+                    children: [
+                      TextSpan(
+                        text: 'ETB 1000',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: ColorConstant.primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text: '/night',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(color: ColorConstant.inActiveColor),
+                      ),
+                    ],
+                  )),
+                ],
+              ),
+            ],
         ),
       ),
     );
