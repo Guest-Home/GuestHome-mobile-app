c6a7616dc68c784debd1123ce55c15220a2e5b84
amanueldemelash12 <amanueldemelash12@gmail.com>
2024-12-28 09:53:46 +0300

    configure dio clent

---
+++ lib/core/apiConstants/api_url.dart
@@ -0,0 +1,6 @@
+class ApiUrl {
+  static const String baseUrl = String.fromEnvironment('BaseUrl');
+  //host
+
+  //guest
+}
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:minapp/core/common/bloc/language_bloc.dart';
+import 'package:minapp/core/network/dio_client.dart';
 import 'package:minapp/features/host/features/properties/presentation/bloc/add_property/add_property_bloc.dart';
 import 'package:minapp/features/onbording/presentation/bloc/on_bording_bloc.dart';
 
@@ -10,4 +11,7 @@ void setup() {
   getIt.registerFactory<LanguageBloc>(() => LanguageBloc());
   getIt.registerFactory<OnBordingBloc>(() => OnBordingBloc());
   getIt.registerFactory<AddPropertyBloc>(() => AddPropertyBloc());
+
+  //dio client
+  getIt.registerSingleton<DioClient>(DioClient());
 }
