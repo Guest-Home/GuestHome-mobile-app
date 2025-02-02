90d230df4eb58b203b74642eff8a512c8e1681b5
amanueldemelash12 <amanueldemelash12@gmail.com>
2025-02-02 03:25:43 +0300

    request pagination

--- lib/features/host/features/request/presentation/pages/request.dart
+++ lib/features/host/features/request/presentation/pages/request.dart
@@ -108,7 +108,6 @@ class _RequestState extends State<Request> {
                               ),
                             )
                         ),]
-                      
                       ),
                     );
                   }
